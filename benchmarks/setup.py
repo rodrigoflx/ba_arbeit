fb9c5a4e6b60a9c306c0657dd5106b4707ecacfd
rodff <rod.forno@gmail.com>
2025-01-21 18:03:27 +0100

    Added new command for benchmarking accuracy

--- benchmarks/setup.py
+++ benchmarks/setup.py
@@ -10,12 +10,15 @@ setup(
         "pandas",
         "JPype1",
         "sortedcontainers",
+        "numpy",
+        "scipy"
     ],
     entry_points={
         'console_scripts': [
             'sample_zipf = CLI:sample_zipf',
             'run_all_benchmarks = CLI:run_all_benchmarks',
             'perf_benchmark = CLI:perf_benchmark',
+            'acc_benchmark = CLI:acc_benchmark',
             'graph_result = CLI:graph_result',
             'graph_results_pairwise = CLI:graph_results_pairwise',
         ],
